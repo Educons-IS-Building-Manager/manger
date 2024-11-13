ecef2872376184b876ae3e8db3c1cad38b964900
Varvara <varvarahomenkova2512@gmail.com>
2024-11-13 19:29:42 +0100

    Initial commit

--- src/main/java/me/manger/model/Apartment.java
+++ src/main/java/me/manger/model/Apartment.java
@@ -1,5 +1,11 @@
 package me.manger.model;
 
-public class Apartment {
+import java.util.ArrayList;
+import me.manger.model.user.User;
 
+
+public class Apartment {
+    private int number;
+    private double area;
+    private ArrayList<User> owners;
 }
