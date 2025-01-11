30452e75e97e3037d95ac955fd0885dd80050d1e
Luka Nedeljkov <luka.nedeljkov@gmail.com>
2025-01-11 18:42:38 +0100

    Added database file structure

---
+++ src/main/java/me/manger/db/FileNames.java
@@ -0,0 +1,17 @@
+package me.manger.db;
+
+public class FileNames {
+
+    // Directories
+    public static final String DATA = "data";
+    public static final String USERS = "data/users";
+    public static final String APARTMENTS = "data/apartments";
+
+    // Files
+    public static final String DATABASE = "data/database.dat";
+    public static final String COMPANY = "data/company.dat";
+    public static final String BUILDING = "data/building.dat";
+    public static final String USER = "data/user.dat";
+    public static final String LEDGER = "data/ledger.dat";
+
+}
